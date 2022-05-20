bd0c74eda770ad6af3e60305dc9848bc548d4e7d
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-05-20 20:22:40 +0100

    self-bootstrapped data

--- src/tuning.rs
+++ src/tuning.rs
@@ -1,25 +1,15 @@
 use std::{fs::File, io::{BufReader, BufRead, BufWriter, Write}};
 
-use crate::{evaluation::EvalVector, errors::FenParseError, board::Board};
+use crate::{board::evaluation::EvalVector, board::Board, searchinfo::SearchInfo};
 
-pub fn eval_vec_for_fen(fen: &str, board: &mut Board) -> Result<EvalVector, FenParseError> {
-    board.set_from_fen(fen)?;
-    if !board.is_quiet_position() {
-        let mut vec_out = EvalVector::new();
-        vec_out.valid = false;
-        return Ok(vec_out);
-    }
-    Ok(board.evaluate_recorded())
-}
+// pub fn eval_positions(fens: &[&str]) -> Vec<EvalVector> {
+//     let mut board = Board::new();
+//     fens.iter()
+//         .filter_map(|fen| eval_vec_for_fen(fen, &mut board).ok())
+//         .collect()
+// }
 
-pub fn eval_positions(fens: &[&str]) -> Vec<EvalVector> {
-    let mut board = Board::new();
-    fens.iter()
-        .filter_map(|fen| eval_vec_for_fen(fen, &mut board).ok())
-        .collect()
-}
-
-pub fn annotate_positions(input_fname: &str, output_fname: &str) {
+pub fn annotate_positions(input_fname: &str, output_fname: &str, n_positions: usize, depth: usize) {
     #![allow(clippy::cast_possible_truncation)]
     let i = File::open(input_fname).unwrap();
     let reader = BufReader::new(i);
@@ -28,25 +18,31 @@ pub fn annotate_positions(input_fname: &str, output_fname: &str) {
     let mut board = Board::new();
     writer.write_all(format!("eval,{}\n", EvalVector::header()).as_bytes()).unwrap();
     let mut counter = 0;
-    for line in reader.lines().skip(1) {
-        if counter % 1000 == 0 {
-            println!("{}                   \r", counter);
-        }
-        let line = line.unwrap();
-        let (fen, evalstr) = line.split_once(',').unwrap();
-        if evalstr.contains('#') { continue; }
-        let evalstr = evalstr.trim_matches(|c| c == ' ' || c == '\n' || c == '+');
-        let eval = evalstr.parse::<i32>();
-        if eval.is_err() { println!("{}", evalstr); continue; }
-        let eval = eval.unwrap();
-        if eval.abs() > 1500 { continue; }
-        let eval_millipawns = eval * 10;
-        let eval_vec = eval_vec_for_fen(fen, &mut board).unwrap();
-        if eval_vec.valid {
-            let output = format!("{},{}\n", eval_millipawns, eval_vec.csvify());
-            writer.write_all(output.as_bytes()).unwrap();
+    let mut info = SearchInfo { depth, ..SearchInfo::default() };
+    for line in reader.lines().skip(1).take(n_positions) {
+        if counter % 10 == 0 {
+            print!("{}                   \r", counter);
         }
         counter += 1;
+        let line = line.unwrap();
+        let (fen, _) = line.split_once(',').unwrap();
+
+        if board.set_from_fen(fen).is_err() {
+            println!("{}", fen);
+            continue;
+        }
+        if !board.is_quiet_position() {
+            continue;
+        }
+        let eval_vec = board.eval_vector();
+        if eval_vec.valid {
+            let eval = board.search_position::<false>(&mut info);
+            if eval.abs() > 700 { continue; }
+            let eval_positional = eval - eval_vec.material_pst;
+            let output = format!("{},{}\n", eval_positional, eval_vec.csvify());
+            writer.write_all(output.as_bytes()).unwrap();
+        }
     }
-    println!("{}                   \r", counter);
+    print!("{}                   \r", counter);
+    println!();
 }
