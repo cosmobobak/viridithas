967de7d7414fa5587b4988fb8195c32c356e1e47
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-06-30 19:28:34 +0100

    eval parameter reading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 #![allow(clippy::if_not_else)]
 
+use board::evaluation::Parameters;
+
 #[macro_use]
 mod macros;
 
@@ -33,15 +35,22 @@ fn main() {
     // takes about 3ms to generate the attack tables on boot
     magic::initialise();
 
-    match args.get(1).map(std::string::String::as_str) {
-        None | Some("uci") => uci::main_loop(),
+    match args.get(1).map(String::as_str) {
+        None | Some("uci") => {
+            let evaluation_parameters = args.get(2).map_or_else(
+                Parameters::default,
+                |path| {
+                    Parameters::from_file(path).unwrap()
+                });
+            uci::main_loop(evaluation_parameters);
+        }
         Some("perfttest") => perft::gamut(),
         Some("tune") => texel::tune(),
         Some("info") => {
             println!("{}", NAME);
             println!(
                 "evaluation parameters: {}",
-                board::evaluation::Parameters::default().vectorise().len()
+                Parameters::default().vectorise().len()
             );
             println!("TT buckets: {}", transpositiontable::DEFAULT_TABLE_SIZE);
             println!(
@@ -53,20 +62,22 @@ fn main() {
         }
         Some("visparams") => {
             let path = args.get(2);
-            let params = path.map_or_else(board::evaluation::Parameters::default, |path| {
-                board::evaluation::Parameters::from_file(path).unwrap()
+            let params = path.map_or_else(Parameters::default, |path| {
+                Parameters::from_file(path).unwrap()
             });
             println!("{params}");
         }
         Some("vispst") => {
             let path = args.get(2);
-            let pst = path.map_or_else(piecesquaretable::sftables::construct_sf_pst, |path| {
-                let params = board::evaluation::Parameters::from_file(path).unwrap();
+            let params = path.map_or_else(Parameters::default, |path| {
+                Parameters::from_file(path).unwrap()
+            });
+            let pst = {
                 let mut board = board::Board::new();
                 board.set_eval_params(params);
                 *board.get_pst()
-            });
-            piecesquaretable::_render_pst_table(&pst);
+            };
+            piecesquaretable::render_pst_table(&pst);
         }
         Some(unknown) => {
             if unknown != "help" {
@@ -78,6 +89,7 @@ fn main() {
             println!(" - tune : use texel's tuning method to optimise the evaluation parameters");
             println!(" - info : miscellaneous information about the engine");
             println!(" - visparams : visualise the evaluation parameters");
+            println!(" - vispst : visualise the piece square tables");
         }
     }
 }
