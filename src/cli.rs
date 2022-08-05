9261566e80c037852b8ddd7a7f642b4ee9b8a52a
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-08-05 14:40:13 +0100

    better cli messages

--- src/cli.rs
+++ src/cli.rs
@@ -4,26 +4,25 @@ use clap::Parser;
 #[clap(author, version, about)]
 #[allow(clippy::struct_excessive_bools)]
 pub struct Cli {
-    /// A path to the evaluation parameters for the engine.
-    /// If omitted, the default parameters will be used.
+    /// A path to the evaluation parameters for the engine - if omitted, the default parameters will be used
     #[clap(long, value_parser, value_name = "PATH")]
     pub params: Option<std::path::PathBuf>,
-    /// Generate source code for PSQTs based on an evaluation parameters file.
+    /// Generate source code for PSQTs based on the evaluation parameters
     #[clap(long, value_parser, value_name = "PATH")]
     pub gensource: bool,
-    /// Run the perft test suite.
+    /// Run the perft test suite
     #[clap(long)]
     pub perfttest: bool,
-    /// Run the texel tuner.
+    /// Run the texel tuner
     #[clap(long)]
     pub tune: bool,
-    /// Display misc. information about the engine.
+    /// Display misc. information about the engine
     #[clap(short, long)]
     pub info: bool,
-    /// Visualise the evaluation parameters.
+    /// Visualise the evaluation parameters
     #[clap(long)]
     pub visparams: bool,
-    /// Visualise the Piece-Square Tables.
+    /// Visualise the Piece-Square Tables
     #[clap(long)]
     pub vispsqt: bool,
 }
