b13adcf07006aad67e06bf04d5501b9dd5016b16
Cosmo Bobak <s2079150@selby.inf.ed.ac.uk>
2022-08-08 16:35:40 +0100

    data generation tracks eval now

--- selfplaydatagen.sh
+++ selfplaydatagen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nice -n 10 ~/cutechess-cli-folder/cutechess-cli -engine cmd=target/release/viridithas name=dev -engine cmd=target/release/viridithas name=dev2 -each timemargin=400 proto=uci tc=100/8+0.08 -rounds 300000 -concurrency 60 -openings file=../uhobook.pgn format=pgn -repeat -pgnout selfplay.pgn min fi > /dev/null
+nice -n 10 ~/cutechess-cli-folder/cutechess-cli -engine cmd=target/release/viridithas name=dev -engine cmd=target/release/viridithas name=dev2 -each timemargin=400 proto=uci tc=100/8+0.08 -rounds 300000 -concurrency 60 -openings file=../uhobook.pgn format=pgn -pgnout selfplay.pgn fi > /dev/null
