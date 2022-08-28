64feb70b7170c637efaf5517f9cac241be2eb949
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-08-28 14:09:28 +0100

    bugfix

--- src/board/evaluation.rs
+++ src/board/evaluation.rs
@@ -167,7 +167,7 @@ pub static PASSED_PAWN_BONUS: [S; 6] = [
 
 pub const TEMPO_BONUS: S = S(2, 5);
 
-pub const COLOUR_COMPLEX_MULTIPLIER: S = S(2, 2);
+pub const COLOUR_COMPLEX_MULTIPLIER: S = S(100, 100);
 
 const KING_DANGER_COEFFS: [i32; 3] = [38, 199, -739];
 
