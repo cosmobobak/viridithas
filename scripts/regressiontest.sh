ea153649e43ffb4b010890fb9ecdfc096f93c481
Cosmo Bobak <s2079150@selby.inf.ed.ac.uk>
2022-09-07 17:19:34 +0100

    script for testing changes quickly

---
+++ scripts/regressiontest.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+nice -n 10 cutechess-cli -engine cmd=target/release/viridithas name=$1 -engine cmd=dev name=dev -each timemargin=400 proto=uci tc=100/8+0.08 -rounds 5000 -concurrency 60 -openings file=../uhobook.pgn format=pgn -repeat -games 2
