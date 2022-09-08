8202ea489119a51f6b1c901036fdc79b97c42667
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-09-08 01:17:33 +0100

    speculative aspiration fix

--- src/board.rs
+++ src/board.rs
@@ -1279,7 +1279,12 @@ impl Board {
 
                 break; // we got an exact score, so we can stop the aspiration loop.
             }
-            aspiration_window = AspirationWindow::from_last_score(most_recent_score);
+
+            if i_depth > 4 {
+                aspiration_window = AspirationWindow::from_last_score(most_recent_score);
+            } else {
+                aspiration_window = AspirationWindow::new();
+            }
         }
 
         if info.print_to_stdout {
