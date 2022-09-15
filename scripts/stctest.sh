0bc0b76d729b2f3a843e4fb634fb7c5c52bb4a45
Cosmo Bobak <s2079150@selby.inf.ed.ac.uk>
2022-09-16 00:09:55 +0100

    add scripts for proper regression testing

--- scripts/stctest.sh
+++ scripts/stctest.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 nice -n 10 cutechess-cli -engine cmd=target/release/viridithas name=$1 -engine cmd=dev name=dev -each timemargin=400 proto=uci tc=100/8+0.08 -rounds 2500 -concurrency 60 -openings file=../uhobook.pgn format=pgn -repeat -games 2
+echo "Test done at 8s+0.08s"
