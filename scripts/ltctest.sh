49bb9e59aedc77e42e3c256eb0dcf87d931876e7
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-09-18 18:36:27 +0100

    randomise openings

--- scripts/ltctest.sh
+++ scripts/ltctest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nice -n 10 cutechess-cli -engine cmd=target/release/viridithas name=$1 -engine cmd=dev name=dev -each timemargin=400 proto=uci tc=100/600+1 -rounds 2500 -concurrency 60 -openings file=../uhobook.pgn format=pgn -repeat -games 2 -pgnout ltctests.pgn
+nice -n 10 cutechess-cli -engine cmd=target/release/viridithas name=$1 -engine cmd=dev name=dev -each timemargin=400 proto=uci tc=100/600+1 -rounds 2500 -concurrency 60 -openings file=../uhobook.pgn order=random format=pgn -repeat -games 2 -pgnout ltctests.pgn -event LTC_REGRESSION_TEST
 echo "Test done at 10m+1s"
