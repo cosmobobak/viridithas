c0df1490eb9b038e24a6e78e0cf5ded5e8065791
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2022-10-14 16:08:14 +0100

    possible fix

--- src/threadlocal.rs
+++ src/threadlocal.rs
@@ -1,4 +1,4 @@
-use crate::{nnue, chessmove::Move, definitions::MAX_PLY};
+use crate::{chessmove::Move, definitions::MAX_PLY, nnue};
 
 pub struct ThreadData {
     pub evals: [i32; MAX_PLY],
@@ -9,8 +9,8 @@ pub struct ThreadData {
 
 impl ThreadData {
     pub fn new() -> Self {
-        Self { 
-            evals: [0; MAX_PLY], 
+        Self {
+            evals: [0; MAX_PLY],
             excluded: [Move::NULL; MAX_PLY],
             nnue: Box::new(nnue::NNUEState::new()),
             root_moves: Vec::new(),
@@ -22,7 +22,16 @@ impl ThreadData {
         let nodecount = |m| nodecounts.iter().find(|(m2, _)| *m2 == m).map(|(_, n)| *n);
         // moves with the highest nodecounts at the front, please.
         for entry in &mut self.root_moves {
-            entry.1 = nodecount(entry.0).unwrap_or(entry.1);
+            let nc = nodecount(entry.0);
+            entry.1 = if let Some(nc) = nc {
+                if nc == 0 {
+                    entry.1
+                } else {
+                    nc
+                }
+            } else {
+                entry.1
+            };
         }
         self.root_moves.sort_by_key(|b| std::cmp::Reverse(b.1));
         // for entry in &self.root_moves {
