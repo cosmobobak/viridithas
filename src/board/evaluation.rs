3b6a07299e1e7675de56297ce5c781b96675072f
Cosmo Bobak <cosmobobak@gmail.com>
2022-11-10 17:49:12 +0000

    more complexity

--- src/board/evaluation.rs
+++ src/board/evaluation.rs
@@ -204,7 +204,7 @@ impl Board {
         let psqt = self.pst_vals.value(self.phase());
         let complexity = (v - psqt).abs();
         
-        v + complexity / 20
+        v + complexity / 10
     }
 
     pub fn evaluate<const USE_NNUE: bool>(&self, t: &mut ThreadData, nodes: u64) -> i32 {
