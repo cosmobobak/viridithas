a3dfb15c049e355ba678b3b4669763f4b88d65d6
Cosmo Bobak <cosmobobak@gmail.com>
2022-12-17 14:13:30 +0000

    remove bad LMR

--- src/search.rs
+++ src/search.rs
@@ -448,7 +448,6 @@ impl Board {
                 {
                     let mut r = self.lmr_table.getr(depth, moves_made);
                     r += i32::from(!PV);
-                    r += i32::from(in_check && type_of(self.moved_piece(m)) == KING);
                     Depth::new(r).clamp(ONE_PLY, depth - 1)
                 } else {
                     ONE_PLY
