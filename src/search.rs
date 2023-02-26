851785c11a2870a313647e83fb6c1ebd448d1982
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2023-02-26 22:16:24 +0000

    add LMR back

--- src/search.rs
+++ src/search.rs
@@ -804,7 +804,16 @@ impl Board {
                 }
             } else {
                 // calculation of LMR stuff
-                let r = ONE_PLY;
+                let r = if (is_quiet || !is_winning_capture)
+                    && depth >= Depth::new(3)
+                    && moves_made >= (2 + usize::from(PV))
+                {
+                    let mut r = get_lm_table().lm_reduction(depth, moves_made);
+                    r += i32::from(!PV);
+                    Depth::new(r).clamp(ONE_PLY, depth - 1)
+                } else {
+                    ONE_PLY
+                };
                 // perform a zero-window search
                 score = -self.zw_search::<NNUE>(
                     tt,
