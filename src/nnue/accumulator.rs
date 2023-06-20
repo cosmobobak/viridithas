1ad9aa84b58ef3c5321dfd5007505e067b561cd1
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2023-06-20 11:54:14 +0100

    optimise accumulator refreshes

--- src/nnue/accumulator.rs
+++ src/nnue/accumulator.rs
@@ -1,6 +1,6 @@
 // use crate::{board::Board, piece::PieceType};
 
-use super::network::Align64;
+use super::network::{Align64, Update};
 
 /// Activations of the hidden layer.
 #[derive(Debug, Clone, Copy)]
@@ -11,8 +11,8 @@ pub struct Accumulator<const HIDDEN: usize> {
 
 impl<const HIDDEN: usize> Accumulator<HIDDEN> {
     /// Initializes the accumulator with the given bias.
-    pub fn init(&mut self, bias: &Align64<[i16; HIDDEN]>) {
-        self.white = *bias;
-        self.black = *bias;
+    pub fn init(&mut self, bias: &Align64<[i16; HIDDEN]>, update: Update) {
+        if update.white { self.white = *bias; }
+        if update.black { self.black = *bias; }
     }
 }
