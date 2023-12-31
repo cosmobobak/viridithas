b609e7214f32bb18e412c7099f7e6e4c46b8aad2
cosmobobak <cosmobobak@gmail.com>
2023-12-31 23:02:56 +0000

    dud patch to update bench
    Bench: 16013105

--- scripts/bench-against.sh
+++ scripts/bench-against.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nice -n 10 cutechess-cli -engine cmd=target/release/viridithas stderr=regteststderr.txt -engine cmd=$1 -each timemargin=400 proto=uci tc=8+0.08 option.Hash=16 -rounds 2500 -concurrency 60 -openings file=../uhobook.pgn order=random format=pgn -repeat -games 2 -pgnout stctests.pgn -event STC_REGRESSION_TEST
+nice -n 10 cutechess-cli -engine cmd=~/personal/viridithas/target/release/viridithas -engine cmd=$1 -each timemargin=400 proto=uci tc=8+0.08 option.Hash=16 -rounds 2500 -concurrency 12 -openings file=book.epd order=random format=epd -repeat -games 2 -pgnout stctests.pgn -event STC_REGRESSION_TEST
 echo "Test done at 8s+0.08s"
