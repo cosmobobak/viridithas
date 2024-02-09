7666c0fb17af63ba1b82578f4ca24326bd93bc34
cosmobobak <cosmobobak@gmail.com>
2024-02-09 16:25:17 +0000

    use ArrayVec for principal variation storage.
    Bench: 15699820

--- src/search/pv.rs
+++ src/search/pv.rs
@@ -1,25 +1,26 @@
 use std::fmt::Display;
 
+use arrayvec::ArrayVec;
+
 use crate::util::MAX_PLY;
 
 use crate::chessmove::Move;
 
 #[derive(Clone, Debug)]
 pub struct PVariation {
-    pub(crate) length: usize,
     pub(crate) score: i32,
-    pub(crate) line: [Move; MAX_PLY],
+    pub(crate) moves: ArrayVec<Move, MAX_PLY>,
 }
 
 impl Default for PVariation {
     fn default() -> Self {
-        Self { length: 0, score: 0, line: [Move::NULL; MAX_PLY] }
+        Self { score: 0, moves: ArrayVec::new() }
     }
 }
 
 impl PVariation {
     pub fn moves(&self) -> &[Move] {
-        &self.line[..self.length]
+        &self.moves
     }
 
     pub const fn score(&self) -> i32 {
@@ -27,15 +28,15 @@ impl PVariation {
     }
 
     pub(crate) fn load_from(&mut self, m: Move, rest: &Self) {
-        self.line[0] = m;
-        self.line[1..=rest.length].copy_from_slice(&rest.line[..rest.length]);
-        self.length = rest.length + 1;
+        self.moves.clear();
+        self.moves.push(m);
+        self.moves.try_extend_from_slice(&rest.moves).expect("attempted to construct a PV longer than MAX_PLY.");
     }
 }
 
 impl Display for PVariation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.length != 0 {
+        if !self.moves.is_empty() {
             write!(f, "pv ")?;
         }
         for &m in self.moves() {
