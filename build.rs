1c3a5f24662310c20f3641f7e1292e73614965fc
cosmobobak <cosmobobak@gmail.com>
2024-06-12 21:12:52 +0100

    fix for new-OB network paths.
    Bench: 17336738

--- build.rs
+++ build.rs
@@ -1,9 +1,22 @@
-#[cfg(any(feature = "bindgen", feature = "syzygy"))]
 use std::env;
 
 fn main() {
+    prep_net();
     build_dependencies();
     generate_bindings();
+}
+
+fn prep_net() {
+    let net_path = env::var("EVALFILE").unwrap_or("viridithas.nnue".into());
+    if net_path == "viridithas.nnue" {
+        // check if net exists
+        if let Err(e) = std::fs::metadata(net_path) {
+            eprintln!("Couldn't read default net during build script! {e}");
+            eprintln!("Note: viri looks for a default net in the project root called \"viridithas.nnue\".");
+        }
+        return;
+    }
+    std::fs::copy(net_path, "viridithas.nnue").unwrap();
 }
 
 fn build_dependencies() {
@@ -35,7 +48,7 @@ fn generate_bindings() {
 fn generate_fathom_bindings() {
     let bindings = bindgen::Builder::default()
         .header("./deps/fathom/src/tbprobe.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .layout_tests(false)
         .generate()
         .unwrap();
--- src/nnue/network.rs
+++ src/nnue/network.rs
@@ -270,8 +270,7 @@ impl<T, const SIZE: usize> DerefMut for Align64<[T; SIZE]> {
 // read in the binary file containing the network parameters
 // have to do some path manipulation to get relative paths to work
 // SAFETY: alignment to u16 is guaranteed because transmute() is a copy operation.
-pub static NNUE: NNUEParams =
-    unsafe { mem::transmute(*include_bytes!(concat!("../../", env!("EVALFILE"),))) };
+pub static NNUE: NNUEParams = unsafe { mem::transmute(*include_bytes!("../../viridithas.nnue")) };
 
 #[repr(C)]
 pub struct NNUEParams {
