0ac73260cb9a824cf3ff3d9922d0ac026a85c0f3
cosmobobak <cosmobobak@gmail.com>
2024-10-24 19:54:14 +0100

    add benchmark script
    Bench: 5703931

---
+++ scripts/run_benchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# name of the cargo package (substitute '-' with '_'!)
+NAME=viridithas
+
+# run the command given in the arguments and collect performance statistics
+perf record --call-graph dwarf target/release/viridithas bench
+
+# process the performance statistics:
+# map the first occurrence of either `even` or `odd` in a flame to `eveod`, and drop the rest
+perf script | inferno-collapse-perf | sed \
+  -e 's/;viridithas::search::<impl viridithas::board::Board>::alpha_beta/;viridithas::search::<impl viridithas::board::Board>::absearch/' \
+  -e 's/;viridithas::search::<impl viridithas::board::Board>::quiescence//g' \
+  -e 's/;viridithas::search::<impl viridithas::board::Board>::alpha_beta//g' > stacks.folded
+
+# create the flamegraph
+cat stacks.folded | inferno-flamegraph > flamegraph.svg
