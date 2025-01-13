8771ac2e1e30a55481718dd71fb65cbe63428694
cosmobobak <cosmobobak@gmail.com>
2025-01-13 21:43:07 +0000

    merge feature planes
    Bench: 5239937

--- src/nnue/network/feature.rs
+++ src/nnue/network/feature.rs
@@ -1,6 +1,6 @@
 use crate::{
     chess::{
-        piece::Colour,
+        piece::{Colour, PieceType},
         types::{File, Square},
     },
     nnue::network::{FeatureUpdate, INPUT},
@@ -22,37 +22,29 @@ impl FeatureIndex {
     }
 }
 
-pub fn indices(white_king: Square, black_king: Square, f: FeatureUpdate) -> [FeatureIndex; 2] {
+pub fn index(colour: Colour, king: Square, f: FeatureUpdate) -> FeatureIndex {
     const COLOUR_STRIDE: usize = 64 * 6;
     const PIECE_STRIDE: usize = 64;
 
-    let white_sq = if white_king.file() >= File::E {
+    let sq = if king.file() >= File::E {
         f.sq.flip_file()
     } else {
         f.sq
-    };
-    let black_sq = if black_king.file() >= File::E {
-        f.sq.flip_file()
-    } else {
-        f.sq
-    };
+    }
+    .relative_to(colour);
 
     let piece_type = f.piece.piece_type().index();
-    let colour = f.piece.colour().index();
+    let colour = (f.piece.colour().index() ^ colour.index()) * usize::from(f.piece.piece_type() != PieceType::King);
 
-    let white_idx = colour * COLOUR_STRIDE + piece_type * PIECE_STRIDE + white_sq.index();
-    let black_idx =
-        (1 ^ colour) * COLOUR_STRIDE + piece_type * PIECE_STRIDE + black_sq.flip_rank().index();
+    let idx = colour * COLOUR_STRIDE + piece_type * PIECE_STRIDE + sq.index();
 
     // SAFETY: important invariant being upheld here!!
-    assert!(
-        white_idx < INPUT && black_idx < INPUT,
-        "attempt to construct illegal FeatureIndex."
-    );
-    [FeatureIndex(white_idx), FeatureIndex(black_idx)]
+    assert!(idx < INPUT, "attempt to construct illegal FeatureIndex.");
+    FeatureIndex(idx)
 }
 
-pub fn index(colour: Colour, king: Square, f: FeatureUpdate) -> FeatureIndex {
+/// For non-merged king planes.
+pub fn index_full(colour: Colour, king: Square, f: FeatureUpdate) -> usize {
     const COLOUR_STRIDE: usize = 64 * 6;
     const PIECE_STRIDE: usize = 64;
 
@@ -68,38 +60,6 @@ pub fn index(colour: Colour, king: Square, f: FeatureUpdate) -> FeatureIndex {
 
     let idx = colour * COLOUR_STRIDE + piece_type * PIECE_STRIDE + sq.index();
 
-    // SAFETY: important invariant being upheld here!!
-    assert!(idx < INPUT, "attempt to construct illegal FeatureIndex.");
-    FeatureIndex(idx)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::chess::piece::Piece;
-
-    use super::*;
-
-    #[test]
-    fn partial_equals_full() {
-        for sq in Square::all() {
-            for piece in Piece::all() {
-                for wk in Square::all() {
-                    for bk in Square::all() {
-                        let f = FeatureUpdate { sq, piece };
-                        let [i1, i2] = indices(wk, bk, f);
-                        assert_eq!(
-                            i1.index(),
-                            index(Colour::White, wk, f).index(),
-                            "Failure when trying WHITE ({f:?}, {wk:?}, {bk:?}) - {i1:?} != {i2:?}"
-                        );
-                        assert_eq!(
-                            i2.index(),
-                            index(Colour::Black, bk, f).index(),
-                            "Failure when trying BLACK ({f:?}, {wk:?}, {bk:?}) - {i1:?} != {i2:?}"
-                        );
-                    }
-                }
-            }
-        }
-    }
+    assert!(idx < 12 * 64);
+    idx
 }
