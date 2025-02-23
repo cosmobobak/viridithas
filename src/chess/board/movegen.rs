b98aca5c7a74eecc5c1624c83bae65af16080490
cosmobobak <cosmobobak@gmail.com>
2025-02-23 17:20:23 +0000

    Remove check to play nice with noob workers
    Bench: 7790371

--- src/chess/board/movegen.rs
+++ src/chess/board/movegen.rs
@@ -228,7 +228,7 @@ pub fn init_sliders_attacks() -> anyhow::Result<()> {
 
 #[allow(clippy::cast_possible_truncation)]
 pub fn bishop_attacks(sq: Square, blockers: SquareSet) -> SquareSet {
-    const _INDEX_LEGAL: () = assert!(1 << BISHOP_REL_BITS == BISHOP_ATTACKS[0].len());
+    // const _INDEX_LEGAL: () = assert!(1 << BISHOP_REL_BITS == BISHOP_ATTACKS[0].len());
     let entry = &BISHOP_TABLE[sq];
     let relevant_blockers = blockers & entry.mask;
     let data = relevant_blockers.inner().wrapping_mul(entry.magic);
@@ -241,7 +241,7 @@ pub fn bishop_attacks(sq: Square, blockers: SquareSet) -> SquareSet {
 }
 #[allow(clippy::cast_possible_truncation)]
 pub fn rook_attacks(sq: Square, blockers: SquareSet) -> SquareSet {
-    const _INDEX_LEGAL: () = assert!(1 << ROOK_REL_BITS == ROOK_ATTACKS[0].len());
+    // const _INDEX_LEGAL: () = assert!(1 << ROOK_REL_BITS == ROOK_ATTACKS[0].len());
     let entry = &ROOK_TABLE[sq];
     let relevant_blockers = blockers & entry.mask;
     let data = relevant_blockers.inner().wrapping_mul(entry.magic);
