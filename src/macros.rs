b13a76af1c0f47f448b8090e66998c3e0b7a7b78
cosmobobak <cosmobobak@gmail.com>
2025-10-04 22:23:50 +0100

    Add more metrics to `track!` macro.
    Bench: 8949666

--- src/macros.rs
+++ src/macros.rs
@@ -40,19 +40,30 @@ macro_rules! track {
                 clippy::cast_sign_loss,
                 clippy::cast_possible_wrap
             )]
-            static TOTAL: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(0);
             static COUNT: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(0);
+            static TOTAL: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(0);
+            static TOTAL_ABS: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(0);
+            static TOTAL_SQ: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(0);
             static MAX: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(i64::MIN);
             static MIN: std::sync::atomic::AtomicI64 = std::sync::atomic::AtomicI64::new(i64::MAX);
             let v = $v as i64;
             MAX.fetch_max(v, std::sync::atomic::Ordering::Relaxed);
             MIN.fetch_min(v, std::sync::atomic::Ordering::Relaxed);
             TOTAL.fetch_add(v, std::sync::atomic::Ordering::Relaxed);
+            TOTAL_ABS.fetch_add(v.abs(), std::sync::atomic::Ordering::Relaxed);
+            TOTAL_SQ.fetch_add(v * v, std::sync::atomic::Ordering::Relaxed);
             let count = COUNT.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
             if count % 8192 == 0 {
                 let total = TOTAL.load(std::sync::atomic::Ordering::Relaxed);
                 let avg = total as f64 / count as f64;
                 println!("average value of {}: {}", stringify!($v), avg);
+                let total_abs = TOTAL_ABS.load(std::sync::atomic::Ordering::Relaxed);
+                let avg_abs = total_abs as f64 / count as f64;
+                println!("average absolute value of {}: {}", stringify!($v), avg_abs);
+                let total_sq = TOTAL_SQ.load(std::sync::atomic::Ordering::Relaxed);
+                let variance = total_sq as f64 / count as f64 - avg * avg;
+                let stddev = variance.sqrt();
+                println!("stddev of {}: {}", stringify!($v), stddev);
                 println!(
                     "min/max value of {}: {}/{}",
                     stringify!($v),
