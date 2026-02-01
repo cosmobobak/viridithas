3048b9076b680e8f180011a4152ddbcdb92cca8d
cosmobobak <cosmobobak@gmail.com>
2026-02-01 01:14:41 +0000

    dce
    Bench: 5066530

--- src/chess/piece.rs
+++ src/chess/piece.rs
@@ -225,10 +225,6 @@ impl Piece {
         unsafe { PieceType::from_index_unchecked(pt_index) }
     }
 
-    pub const fn is_valuable(self) -> bool {
-        self.piece_type().legal_promo()
-    }
-
     pub const fn char(self) -> char {
         match self {
             Self::WP => 'P',
